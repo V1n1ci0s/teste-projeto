a6d1acd21150b939e6f9f212f76f94c03ae509ec
V1n1ci0s <145877406+V1n1ci0s@users.noreply.github.com>
2024-06-04 16:39:17 -0300

    Update testenovo.py

--- testenovo.py
+++ testenovo.py
@@ -288,17 +288,30 @@ st.subheader("Distribuição de Suicídios por País")
 fig = px.histogram(df, x='country', title="Distribuição de Suicídios por País")
 st.plotly_chart(fig)
 
-# Gráfico interativo do mapa do Brasil
-st.header("Mapa do Brasil de Taxas de Suicídio por Estado ao Longo do Tempo")
-df_brasil_map = df[df['country'] == 'Brazil']
+# URL do arquivo GeoJSON com as fronteiras dos estados do Brasil
+url = "https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson"
 
-# Filtrar anos selecionados
-anos_selecionados = st.slider("Selecione o Intervalo de Anos", int(df_brasil_map['year'].min()), int(df_brasil_map['year'].max()), (int(df_brasil_map['year'].min()), int(df_brasil_map['year'].max())))
+# Configurações iniciais
+sns.set_style("dark")
+cor_genero = ['#F781D8', '#819FF7']
+st.set_page_config(layout="wide")
 
-df_brasil_map_filtered = df_brasil_map[(df_brasil_map['year'] >= anos_selecionados[0]) & (df_brasil_map['year'] <= anos_selecionados[1])]
+# Carregar dados
+df = pd.read_csv('https://raw.githubusercontent.com/V1n1ci0s/projeto-Kayo_ter-a/main/base%20dados.csv')
 
+# Títulos e descrições
+st.title("Análise de Suicídios")
+st.write("""
+         Este aplicativo interativo analisa as taxas de suicídio globalmente e no Brasil ao longo do tempo, com base em dados históricos.
+         """)
+
+# Exibir dataframe inicial
+st.header("Dados Iniciais")
+st.write(df.head())
+
+# Carregar GeoJSON das fronteiras dos estados do Brasil diretamente com Plotly Express
 fig = px.choropleth(df_brasil_map_filtered, 
-                    geojson=gdf_brazil_states,  # GeoJSON com as fronteiras dos estados do Brasil
+                    geojson=url,  # GeoJSON com as fronteiras dos estados do Brasil
                     locations='state', 
                     featureidkey="properties.sigla",  # Chave para identificar o estado no GeoJSON
                     color="suicides/100k pop", 
@@ -309,7 +322,3 @@ fig.update_geos(projection_type="mercator")  # Definir a projeção para mercato
 fig.update_layout(coloraxis_colorbar=dict(title="Suicídios/100k Habitantes"),
                   margin={"r":0,"t":30,"l":0,"b":0})
 st.plotly_chart(fig)
-
-
-print(gdf_brazil_states.head())
-print(gdf_brazil_states.info())
